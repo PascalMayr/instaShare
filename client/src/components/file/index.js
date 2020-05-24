dd14be27cc7028b91704fa12cf9a2bb5c95954b5
Pascal <pascalmayr2@gmaill.com>
2020-05-24 17:22:36 +0200

    added status column

--- client/src/components/file/index.js
+++ client/src/components/file/index.js
@@ -25,6 +25,20 @@ const File = (props) => {
       </th>
       <td>{file.contentType}</td>
       <td>{((file.length / 1024) / 1024).toFixed(2)}</td>
+      <td>
+        {
+          status === 200 ? // on status 200 show check
+          <svg className="bi bi-check2-circle" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="lightgreen" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" d="M15.354 2.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L8 9.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+            <path fillRule="evenodd" d="M8 2.5A5.5 5.5 0 1 0 13.5 8a.5.5 0 0 1 1 0 6.5 6.5 0 1 1-3.25-5.63.5.5 0 1 1-.5.865A5.472 5.472 0 0 0 8 2.5z"/>
+          </svg>
+          :  // else show a spinning arrow
+          <svg className="bi bi-arrow-clockwise" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" d="M3.17 6.706a5 5 0 0 1 7.103-3.16.5.5 0 1 0 .454-.892A6 6 0 1 0 13.455 5.5a.5.5 0 0 0-.91.417 5 5 0 1 1-9.375.789z"/>
+            <path fillRule="evenodd" d="M8.147.146a.5.5 0 0 1 .707 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 1 1-.707-.708L10.293 3 8.147.854a.5.5 0 0 1 0-.708z"/>
+          </svg>
+        }
+      </td>
     </tr>
   );
 };
