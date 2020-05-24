13f19480bf752f0fc174e8d87e07af9c172df8a6
Pascal <pascalmayr2@gmaill.com>
2020-05-24 11:06:15 +0200

    updating fileRoutes

--- server/index.js
+++ server/index.js
@@ -2,13 +2,16 @@ const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const userRoutes = require("./routes/user");
+const fileRoutes = require("./routes/file");
 
 // Connecting to MongoDB database
 mongoose
 .connect("mongodb://localhost:27017/instashare", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 .then(() => {
   const app = express()
+  // registering routes
   app.use(userRoutes);
+  app.use(fileRoutes);
 
   app.listen(5000, () => {
     console.log("Server has started!")
