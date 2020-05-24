4746d37f246ad68c9cf14b0243372190185ff957
Pascal <pascalmayr2@gmaill.com>
2020-05-24 11:57:40 +0200

    rendering different homepage when the user  is logged in

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { Jumbotron, Button, Container, Alert } from 'reactstrap';
+import is_user_logged_in from '../../modules/is_user_logged_in'
 
 const Home = () => {
   return (
     <div>
+      {
+        !is_user_logged_in() ?
         <Jumbotron>
           <p className="lead">This is a simple application to upload large files which can be shared instantly.</p>
           <p className="lead">
@@ -19,6 +22,10 @@ const Home = () => {
             </a>
           </p>
         </Jumbotron>
+        :
+        <Container>
+        </Container>
+      }
     </div>
   );
 };
