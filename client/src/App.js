ef6f4525c9a7a16c2c1deef9bc723aca73da881f
Pascal <pascalmayr2@gmaill.com>
2020-05-24 11:18:34 +0200

    added react  router Switch benath  the  header

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
@@ -19,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <Switch>
+      </Switch>
     </div>
   );
 }
