7ea52ffe92ce078f4afb38ad91808284c98a5e8f
Pascal <pascalmayr2@gmaill.com>
2020-05-24 12:27:22 +0200

    added email validity check to the login page

--- client/src/pages/users/login.js
+++ client/src/pages/users/login.js
@@ -5,18 +5,31 @@ import {
   Button, Alert
 } from 'reactstrap';
 import login from '../../modules/login'
+import validate_email from '../../modules/validate_email'
 import action_on_enter from '../../modules/action_on_enter'
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [alert, setAlert] = useState('');
 
   const validate = function(){
+    validate_email(email, (validity) => {
+      if(validity){
+        setAlert('')
         login(email, password, (errorMessage, error) => setAlert(errorMessage))
+  }
+      else{
+        setAlert('Please enter a valid email!')
+      }
+    })
   }
   return (
   <main>
   <Container>
+    {
+      alert !== '' && <Alert color="danger">{alert}</Alert>
+    }
     <h2>Sign In</h2>
     <Form className="form">
       <Col>
