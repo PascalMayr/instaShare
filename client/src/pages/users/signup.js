d5c6ecf7a171fd0de4fc28e8a672837428054089
Pascal <pascalmayr2@gmaill.com>
2020-05-24 12:28:57 +0200

    created validations for the signup  form

--- client/src/pages/users/signup.js
+++ client/src/pages/users/signup.js
@@ -5,20 +5,50 @@ import {
   Button, Alert
 } from 'reactstrap';
 import signup from '../../modules/signup'
+import validate_email from '../../modules/validate_email'
 import action_on_enter from '../../modules/action_on_enter'
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordRepeat, setPasswordRepeat] = useState('');
+  const [alert, setAlert] = useState('');
+  // client  side validations
+  const validatePassword = function(){
+    if(passwordRepeat !== '' && password !== '' && passwordRepeat !== password){
+      setAlert('password must be the same!')
+      return false
+    }
+    if(password === '' || passwordRepeat === ''){
+      setAlert('password must be set!')
+      return false
+    }
+    else{
+      setAlert('')
+      return true
+    }
+  };
  
   const validate = function(){
+    validate_email(email, (validity) => {
+      if(validity){
+        setAlert('')
+        if(validatePassword()){
           signup(email, password, (errorMessage, error) => setAlert(errorMessage))
+        }
+      }
+      else{
+        setAlert('Please enter a valid email!')
+      }
+    })
   }
   
   return (
   <main>
   <Container>
+  {
+    alert !== '' && <Alert color="danger">{alert}</Alert>
+  }
     <h2>Sign up</h2>
     <Form className="form">
       <Col>
