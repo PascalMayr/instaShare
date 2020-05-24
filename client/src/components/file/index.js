12383a30d049807ef0977926a35eb8c2904974ce
Pascal <pascalmayr2@gmaill.com>
2020-05-24 17:03:43 +0200

    created input to change the filename on user input

--- client/src/components/file/index.js
+++ client/src/components/file/index.js
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
+import { Button } from 'reactstrap';
+import update_metadata from '../../modules/update_metadata'
 
 const File = (props) => {
+  const { file } = props
+  const [filename, setFilename] =  useState(file.filename)
+  const [status, setStatus] =  useState(200)
   return (
-    <div>
-
-
-    </div>
+    <tr>
+      <th>
+        { /* let the user change the filename */}
+      <input 
+        placeholder="change filename"
+        className="form-control"
+        value={filename}
+        onChange={e => {
+          update_metadata(file._id, e.target.value)
+          setStatus(202)
+          setFilename(e.target.value);
+          // user feedback
+          setTimeout(() => setStatus(200), 1000)
+        }}
+      />
+      </th>
+    </tr>
   );
 };
 
