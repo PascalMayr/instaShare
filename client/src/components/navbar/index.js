e7460c302688605b52f46bcb17735a6a2f314a56
Pascal <pascalmayr2@gmaill.com>
2020-05-24 12:30:20 +0200

    updating navbar if user is logged in

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -10,10 +10,19 @@ import {
   NavLink,
   NavbarText
 } from 'reactstrap';
+import get_user from '../../modules/get_user'
+import is_user_logged_in from '../../modules/is_user_logged_in'
+
 const Navigation = (props) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [user, setUser] = useState(undefined)
 
   const toggle = () => setIsOpen(!isOpen);
+  
+  is_user_logged_in((jwt, user) => get_user(jwt, user, (data) => {
+    setUser(data.user.email)
+  })) // set user email if user is logged in
+  
   return (
     <div>
       <Navbar color="light" light expand="md">
@@ -21,13 +30,27 @@ const Navigation = (props) => {
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
+            {
+              !is_user_logged_in() ?
+              <React.Fragment>
                 <NavItem>
                   <NavLink href="/users/">Signup</NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink href="/users/login">Login</NavLink>
                 </NavItem>
+              </React.Fragment>
+              :
+              <React.Fragment>
+                <NavItem>
+                  <NavLink href="/" onClick={() => sessionStorage.clear()}>Logout</NavLink>
+                </NavItem>
+              </React.Fragment>
+            }
           </Nav>
+          {
+            user && <NavbarText>{user}</NavbarText>
+          }
         </Collapse>
       </Navbar>
     </div>
