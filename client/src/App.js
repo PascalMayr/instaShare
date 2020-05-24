0105ea693b9d1df8c0b39f463f62e7c0bec42399
Pascal <pascalmayr2@gmaill.com>
2020-05-24 11:46:43 +0200

    created signup form

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom'
 import Navigation from './components/navbar'
 import Home from './pages/home'
 import Login from './pages/users/login'
+import SignUp from './pages/users/signup'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       </header>
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route exact path='/users/' component={SignUp}/>
         <Route exact path='/users/login/' component={Login}/>
       </Switch>
     </div>
---
+++ client/src/pages/users/signup.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import {
+  Container, Col, Form,
+  FormGroup, Label, Input,
+  Button, Alert
+} from 'reactstrap';
+import action_on_enter from '../../modules/action_on_enter'
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordRepeat, setPasswordRepeat] = useState('');
+ 
+  const validate = function(){
+    
+  }
+  
+  return (
+  <main>
+  <Container>
+    <h2>Sign up</h2>
+    <Form className="form">
+      <Col>
+        <FormGroup>
+          <Label>Email</Label>
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="myemail@email.com"
+            onChange={e => setEmail(e.target.value)}
+            onKeyDown={e =>  action_on_enter(e, validate)}
+          />
+        </FormGroup>
+      </Col>
+      <Col>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="********"
+            onChange={e => setPassword(e.target.value)}
+            onKeyDown={e =>  action_on_enter(e, validate)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="passwordRepeat">Repeat Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id="passwordRepeat"
+            placeholder="********"
+            onChange={e => setPasswordRepeat(e.target.value)}
+            onKeyDown={e =>  action_on_enter(e, validate)}
+          />
+        </FormGroup>
+      </Col>
+      <Button onClick={validate}>Submit</Button>
+    </Form>
+  </Container>
+  </main>
+  );
+};
+
+export default SignUp;
