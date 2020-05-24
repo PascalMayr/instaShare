52eaaf446b76f7dfb22c186e60cb01ac535507e4
Pascal <pascalmayr2@gmaill.com>
2020-05-24 11:25:27 +0200

    Added links and name to Navbar

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
 const Navigation = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
   return (
     <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">INSTASHARE</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+                <NavItem>
+                  <NavLink href="/users/">Signup</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/users/login">Login</NavLink>
+                </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
   );
 }
