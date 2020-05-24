48f9575935c2e455e4e7527e8a63c7a49f0950cf
Pascal <pascalmayr2@gmaill.com>
2020-05-24 12:35:02 +0200

    created upload functionality  for signed in users

---
+++ client/src/modules/upload_file/index.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import is_user_logged_in from '../is_user_logged_in'
+import throw_error  from '../throw_error'
+
+const upload_file = (file, callback = () => {}) => {
+  is_user_logged_in((jwt, user) => {
+    var formData = new FormData();
+    formData.append("file1", file);
+    
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Bearer ${jwt}`
+      }
+    }
+
+    axios.post(process.env.REACT_APP_SERVER_HOST + "upload/" + user, formData, config)
+    .then((result) => {
+      callback(result.data)
+      if(result.data.error === undefined){
+        console.log(result.data)
+      }
+      else{
+        throw_error(result.data.error, "couldn't upload file")
+      }
+    })
+    .catch((err) => {
+      throw_error(err, "couldn't upload file")
+    })
+  })
+}
+
+export default upload_file
