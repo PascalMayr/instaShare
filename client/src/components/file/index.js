ddcee2a515aa05b746694b1671638d554c66c9c3
Pascal Mayr <pascalmayr2@gmail.com>
2020-05-24 17:40:10 +0200

    created share action

--- client/src/components/file/index.js
+++ client/src/components/file/index.js
@@ -7,6 +7,7 @@ const File = (props) => {
   const { file } = props
   const [filename, setFilename] =  useState(file.filename)
   const [status, setStatus] =  useState(200)
+  const downloadURI = process.env.REACT_APP_SERVER_HOST + "download/" + file._id
   return (
     <tr>
       <th>
@@ -55,12 +56,32 @@ const File = (props) => {
         </Button>
         {/* download button */}
         <Button color="primary" onClick={e => {
-            window.location.href = process.env.REACT_APP_SERVER_HOST + "download/" + file._id
+            window.location.href = downloadURI
           }}>
           <svg className="bi bi-cloud-download" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M4.887 5.2l-.964-.165A2.5 2.5 0 1 0 3.5 10H6v1H3.5a3.5 3.5 0 1 1 .59-6.95 5.002 5.002 0 1 1 9.804 1.98A2.501 2.501 0 0 1 13.5 11H10v-1h3.5a1.5 1.5 0 0 0 .237-2.981L12.7 6.854l.216-1.028a4 4 0 1 0-7.843-1.587l-.185.96z"/>
             <path fillRule="evenodd" d="M5 12.5a.5.5 0 0 1 .707 0L8 14.793l2.293-2.293a.5.5 0 1 1 .707.707l-2.646 2.646a.5.5 0 0 1-.708 0L5 13.207a.5.5 0 0 1 0-.707z"/>
             <path fillRule="evenodd" d="M8 6a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0v-8A.5.5 0 0 1 8 6z"/>
+          </svg>
+          </Button>
+          {/* share button */}
+          <Button
+            onClick={e => {
+            navigator.clipboard.writeText(downloadURI)
+            .then(() => {
+              let success = 'Link copied to clipboard'
+              props.alertFunction(success)
+              setTimeout(() => {
+                props.alertFunction('')
+              }, 2000)
+              console.log(success);
+            })
+            .catch(() => {
+              console.log('Failed to copy text.');
+            });
+          }}>
+          <svg className="bi bi-reply-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.079 11.9l4.568-3.281a.719.719 0 0 0 0-1.238L9.079 4.1A.716.716 0 0 0 8 4.719V6c-1.5 0-6 0-7 8 2.5-4.5 7-4 7-4v1.281c0 .56.606.898 1.079.62z"/>
           </svg>
           </Button>
       </td>
