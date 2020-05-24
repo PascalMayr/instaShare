2249dc8002a7266d598317ad522a34baf9421898
Pascal Mayr <pascalmayr2@gmail.com>
2020-05-24 17:27:02 +0200

    added delete action button

--- client/src/components/file/index.js
+++ client/src/components/file/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button } from 'reactstrap';
 import update_metadata from '../../modules/update_metadata'
+import delete_file  from '../../modules/delete_file'
 
 const File = (props) => {
   const { file } = props
@@ -38,6 +39,20 @@ const File = (props) => {
             <path fillRule="evenodd" d="M8.147.146a.5.5 0 0 1 .707 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 1 1-.707-.708L10.293 3 8.147.854a.5.5 0 0 1 0-.708z"/>
           </svg>
         }
+      </td>
+      <td className="actions">
+        {/* delete button */}
+        <Button
+          color="danger"
+          onClick={e => {
+            delete_file(file._id);
+            window.location.href = '/'
+          }}
+        >
+          <svg className="bi bi-trash-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+          </svg>
+        </Button>
       </td>
     </tr>
   );
