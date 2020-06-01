5198de5f7cad0dd19603776e49d27dc02d3f3d8f
Pascal Mayr <pascalmayr2@gmail.com>
2020-06-01 12:30:37 +0200

    added chalk

--- server/index.js
+++ server/index.js
@@ -6,12 +6,13 @@ const fileRoutes = require("./routes/file");
 const appRoutes = require("./routes/app");
 const morgan = require('morgan');
 const cors = require('cors');
+const chalk = require('chalk');
 
 // Connecting to MongoDB database
 mongoose
 .connect(process.env.DB_CONNECTION+process.env.DB_NAME, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 .then(() => {
-  console.log('Connected to database!')
+  console.log(chalk.magenta('Connected to database!'))
   
   const app = express()
   app.use(cors()) // enabling cors for all origins should be avoided - TODO: add a specific origin
@@ -31,9 +32,10 @@ mongoose
   const port = process.env.PORT || 5000
 
   app.listen(port, () => {
-    console.log(`Server has started on port: ${port}`)
+    console.log(chalk.green(`Server has started on port: ${port}`))
   })
 })
 .catch(error => {
+  console.log(chalk.red('Could not connect to the database!'))
   console.error(error)
 })
